fbee5d68872d52e20110b3a91fad403594fb130d
Hady <itshadyy@gmail.com>
2025-03-15 01:24:18 +0200

    Booking 03

--- src/pages/authed/CourtBookingPage.vue
+++ src/pages/authed/CourtBookingPage.vue
@@ -131,7 +131,7 @@ const goToSummary = () => {
 
 // Format date
 const formatDate = (date) => {
-    const options = { weekday: 'short', day: 'numeric' };
+    const options = { weekday: 'short', day: 'numeric', month: 'short' };
     return date.toLocaleDateString('en-US', options);
 };
 
